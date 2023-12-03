dde3edbd6174ac108ba7eeba690c3ece2a22cae0
Mars <Mars@DESKTOP-6UQ8GSO>
2023-12-04 01:41:57 +0300

    Добавьте файлы проекта.

---
+++ spasite/Components/Raspredeleniye.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace spasite.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для Raspredeleniye.xaml
+    /// </summary>
+    public partial class Raspredeleniye : Page
+    {
+        public Raspredeleniye()
+        {
+            InitializeComponent();
+        }
+
+
+        private void teacher_Click(object sender, RoutedEventArgs e)
+        {
+ 
+        }
+
+        private void engineer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void departament_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void guest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
